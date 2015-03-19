c93ea56399b0c883a21624b9712157cc8df7475b
ektodorov <ek_todorov@abv.bg>
2015-03-20 00:36:11 +0200

    1.Added fade to the ripple;

--- RippleDrawableComp/src/com/blogspot/techzealous/rippledrawablecomp/RippleDrawableComp.java
+++ RippleDrawableComp/src/com/blogspot/techzealous/rippledrawablecomp/RippleDrawableComp.java
@@ -3,26 +3,32 @@ package com.blogspot.techzealous.rippledrawablecomp;
 import java.lang.ref.WeakReference;
 
 import android.animation.ValueAnimator;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RadialGradient;
 import android.graphics.Rect;
 import android.graphics.Shader.TileMode;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.animation.AccelerateDecelerateInterpolator;
 
 public class RippleDrawableComp extends Drawable {
 
-	//private static final String LOG = "RippleDrawableComp";
+	private static final String LOG = "RippleDrawableComp";
+	private final static int kThresholdStartFade = 30;
+	
 	private WeakReference<View> mWeakView;
 	private Drawable mDrawable;
 	private Paint mPaint;
-	private Paint mPaintInner;
+	private Paint mPaintOverlay;
 	private int mColor;
 	private int mRadius;
 	private int mXDown;
@@ -32,6 +38,10 @@ public class RippleDrawableComp extends Drawable {
 	private int mWidth;
 	private int mHeight;
 	private int mDuration;
+	private int mAlpha;
+	
+	private Bitmap mBitmapOverlay;
+	private Canvas mCanvasOverlay;
 	
 	public RippleDrawableComp(int aColor, int aAlpha, int aInitialRadius, int aDuration, Drawable aDrawable, View aView)
 	{
@@ -39,11 +49,14 @@ public class RippleDrawableComp extends Drawable {
 		mPaint.setColor(aColor);
 		mPaint.setAlpha(aAlpha);
 		mPaint.setAntiAlias(true);
-		mPaintInner = new Paint();
-		mPaintInner.setColor(Color.WHITE);
+		mPaintOverlay = new Paint();
+		//mPaintInner.setColor(Color.WHITE);
+		mPaintOverlay.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+		mCanvasOverlay = new Canvas();
 		mColor = aColor;
 		mRadius = aInitialRadius;
 		mDuration = aDuration;
+		mAlpha = aAlpha;
 		mDrawable = aDrawable;
 		mWeakView = new WeakReference<View>(aView);
 	}
@@ -60,6 +73,9 @@ public class RippleDrawableComp extends Drawable {
 				mWidth = view.getWidth();
 				mHeight = view.getHeight();
 				setBounds(new Rect(0, 0, mWidth, mHeight));
+				
+				mBitmapOverlay = Bitmap.createBitmap(mWidth, mHeight, Config.ARGB_8888);
+				mCanvasOverlay.setBitmap(mBitmapOverlay);
 			}
 		});
 	}
@@ -103,11 +119,13 @@ public class RippleDrawableComp extends Drawable {
 	{
 		RadialGradient rg = new RadialGradient(aX, aY, mRadius, Color.TRANSPARENT, mColor, TileMode.CLAMP);
 		mPaint.setShader(rg);
+		mPaint.setAlpha(mAlpha);
 		
 		mIsRipple = true;
 		mXDown = aX;
 		mYDown = aY;
 		mRadius = 0;
+		
 		ValueAnimator animation = ValueAnimator.ofInt(mRadius, mMaxRadius);
 		animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 			@Override
@@ -116,11 +134,19 @@ public class RippleDrawableComp extends Drawable {
 				int radius = (Integer)aValueAnimator.getAnimatedValue();
 				mRadius = radius;
 				if(mMaxRadius == radius) {mIsRipple = false;}
+				
+				/* Decrease the mPaint's alpha when we are at 70% of the size of the loop. */
+				//we are saving us *100 and /100 when calculating the percents (no need to multiply by 100, just to then delete by 100)
+				float percent = 1.0f - ((float)radius / (float)mMaxRadius);
+				if((int)percent < kThresholdStartFade) {
+					int alpha = (int)(mAlpha * percent);
+					mPaint.setAlpha(alpha);
+				}
+				
 				invalidateSelf();
 			}
 		});
 		animation.setDuration(aDuration);
-		animation.setInterpolator(new AccelerateDecelerateInterpolator());
 		animation.start();
 	}
 	
@@ -130,7 +156,6 @@ public class RippleDrawableComp extends Drawable {
 		if(mIsRipple) {
 			mDrawable.draw(aCanvas);
 			aCanvas.drawCircle(mXDown, mYDown, mRadius, mPaint);
-			//aCanvas.drawCircle(mXDown, mYDown, mRadius - 20, mPaintInner);
 		} else {
 			mDrawable.draw(aCanvas);
 		}
