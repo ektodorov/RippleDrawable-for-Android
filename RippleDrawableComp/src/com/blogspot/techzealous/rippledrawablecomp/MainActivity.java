620beb86c746f9114243ab1ad4ba9798a99c91fb
ektodorov <ek_todorov@abv.bg>
2015-04-02 21:35:38 +0300

    1.Added RippleDrawableComp in separate src directory; 2.Updated example project to include different views and sizes;

--- RippleDrawableComp/src/com/blogspot/techzealous/rippledrawablecomp/MainActivity.java
+++ RippleDrawableComp/src/com/blogspot/techzealous/rippledrawablecomp/MainActivity.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private Button mButton1;
+	private TextView mTextView;
 	private Button mButton2;
+	private RelativeLayout mRelativeLayout;
+	private Button mButton3;
 	
 	private RippleDrawableComp mRipple;
 	
@@ -17,13 +21,30 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mButton1 = (Button)findViewById(R.id.button1);
+		mTextView = (TextView)findViewById(R.id.textView1);
 		mButton2 = (Button)findViewById(R.id.button2);
+		mRelativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout1);
+		mButton3 = (Button)findViewById(R.id.button3);
 		
 		getActionBar().hide();
 		
-		mRipple = new RippleDrawableComp(Color.BLACK, 30, 20, 750, getResources().getDrawable(R.drawable.ic_launcher), mButton2);
+		RippleDrawableComp rippleTextView = new RippleDrawableComp(Color.BLACK, 200, 20, 1000, 
+				getResources().getDrawable(R.drawable.ic_launcher), mTextView);
+		rippleTextView.init();
+		mTextView.setBackgroundDrawable(rippleTextView);
+		
+		mRipple = new RippleDrawableComp(Color.BLACK, 190, 20, 1100, getResources().getDrawable(R.drawable.xml_rect_green), mButton2);
 		mRipple.init();
 		mButton2.setBackgroundDrawable(mRipple);
+		
+		RippleDrawableComp rippleRelativeLayout = new RippleDrawableComp(Color.BLACK, 150, 20, 1200, 
+				getResources().getDrawable(R.drawable.ic_launcher), mRelativeLayout);
+		rippleRelativeLayout.init();
+		mRelativeLayout.setBackgroundDrawable(rippleRelativeLayout);
+		
+		RippleDrawableComp rippleButton3 = new RippleDrawableComp(Color.BLACK, 190, 20, 1100, 
+				getResources().getDrawable(R.drawable.ic_launcher), mButton3);
+		rippleButton3.init();
+		mButton3.setBackgroundDrawable(rippleButton3);
 	}
 }
